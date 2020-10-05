e06c49490f84c0cc3b241410735d253e6b98ac80
TenaKim <tena.kim10@gmail.com>
2020-10-06 06:24:24 +0900

    Check the type casting

--- ft_memset.c
+++ ft_memset.c
@@ -17,14 +17,25 @@
 ** [PROTOTYPE]
 ** #include <string.h>
 ** void		*memset(void *s, int c, size_t n);
-**/
+*/
 
-/*
 void	*ft_memset(void *s, int c, size_t n)
 {
-	unsigned char 
+    size_t i;
+	unsigned char char_c;
+    unsigned char *char_s;
+
+    char_c = c;
+    char_s = s;
+    i = 0;
+    while (i < n)
+    {
+        *char_s++ = char_c;
+        i++;
+    }
+    return (char_s);
 }
-*/
+
 int		main(void)
 {
     printf("====Compare Data Type 'int' & 'unsigned char' For memset====\n");
@@ -41,11 +52,25 @@ int		main(void)
 	for (i = 0; i < 5; i++)
 		printf("nums2[%d] = %d \n", i, nums2[i]);
     printf("\n");
+    
     printf("====Compare Customized ft_memset & Original memset====\n");
-    char chars1[10];
-    char chars2[10];
-    // ft_memset(chars1, 's', sizeof(chars1));
-    // memset(chars1, 's', sizeof(chars2));
-	// printf("ft_memset : %s \nmemset : %s", chars1, chars2);
-	return (0);
+    char chars1[11] = "BornToCode";
+    char chars2[11] = "BornToCode";
+    
+    ft_memset(chars1, 's', sizeof(chars1));
+    memset(chars2, 's', sizeof(chars2));
+    printf("ft_memset : %s\n", chars1);
+    printf("memset : %s\n", chars2);
+    printf("ft_memset size : %ld\n", sizeof(chars1));
+	printf("ft_memset : %s \nmemset : %s\n", chars1, chars2);
+	
+    int idx = 0;
+    printf("chars1\n");
+    while (chars1[idx] != '\0')
+    {
+        printf("%c", chars1[idx]);
+        idx++;
+    }
+    printf("\n");
+    return (0);
 }
