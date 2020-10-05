5ba6da5182285cd52ed729d7b988e4baf736f945
TenaKim <tena.kim10@gmail.com>
2020-10-06 04:39:53 +0900

    Check the difference of data type between int and unsigned char

--- ft_memset.c
+++ ft_memset.c
@@ -12,6 +12,7 @@
 
 #include "libft.h"
 #include <string.h>
+
 /*
 ** [PROTOTYPE]
 ** #include <string.h>
@@ -21,13 +22,30 @@
 /*
 void	*ft_memset(void *s, int c, size_t n)
 {
-
+	unsigned char 
 }
 */
-
 int		main(void)
 {
-	char str[8] = "42seoul";
-	printf("%s", memset(str, 3, 2));
+    printf("====Compare Data Type 'int' & 'unsigned char' For memset====\n");
+	int nums1[5];
+	unsigned char nums2[5];
+	int i;
+
+	memset(nums1, 10, sizeof(nums1));
+	memset(nums2, 10, sizeof(nums2));
+
+	for (i = 0; i < 5; i++)
+		printf("nums1[%d] = %d \n", i, nums1[i]);
+	printf("\n");
+	for (i = 0; i < 5; i++)
+		printf("nums2[%d] = %d \n", i, nums2[i]);
+    printf("\n");
+    printf("====Compare Customized ft_memset & Original memset====\n");
+    char chars1[10];
+    char chars2[10];
+    // ft_memset(chars1, 's', sizeof(chars1));
+    // memset(chars1, 's', sizeof(chars2));
+	// printf("ft_memset : %s \nmemset : %s", chars1, chars2);
 	return (0);
 }
