7160c6e87c29e4a39779ec31b839aa0042a4fb83
TenaKim <tena.kim10@gmail.com>
2020-10-08 03:24:55 +0900

    Finish ft_calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -20,4 +20,31 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
+	size_t		i;
+	void		*res;
+	unsigned char	*char_res;
+
+	res = malloc(count * size);
+	if (!(res))
+		return (NULL);
+	char_res = res;
+	i = 0;
+	while (i < count)
+	{
+		char_res[i] = 0;
+		i++;
+	}
+	return (char_res);
 }
+
+/*
+**int	main(void)
+**{
+**	printf("====ft_calloc====\n");
+**	printf("%p\n", ft_calloc(5, 1));
+**	printf("%ld\n", sizeof(ft_calloc(5, 1)));
+**	printf("====calloc====\n");
+**	printf("%p\n", calloc(5, 1));
+**	printf("%ld\n", sizeof(calloc(5, 1)));
+**}
+*/
