42eacc7a9395a3f5650a727093084f48ef63c549
TenaKim <tena.kim10@gmail.com>
2020-10-08 07:03:56 +0900

    Finish ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -10,4 +10,37 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+/*
+** [PROTOTYPE]
+** #include <string.h>
+** char	*strchr(const char *s, int c);
+*/
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char)c)
+			return ((char *)s + i);
+		i++;
+	}
+	return (NULL);
+}
+
+/*
+**int	main(void)
+**{
+**	// Search the location of character 'e' (decimal 101 on ascii)
+**	// Check the address if the return value is NULL pointer
+**	printf("====ft_strchr====\n");
+**	printf("%s\n%p\n", ft_strchr("42Seoul_Cadet", 101), ft_strchr("42Seoul_Cadet", 5));
+**	printf("====strchr====\n");
+**	printf("%s\n%p\n", strchr("42Seoul_Cadet", 101), strchr("42Seoul_Cadet", 5));
+**	return (0);
+**}
+*/
