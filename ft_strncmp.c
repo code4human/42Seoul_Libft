03542b0b9d5b41b167406f3c8fb7d6555e5b5c23
TenaKim <tena.kim10@gmail.com>
2020-10-08 11:01:47 +0900

    Finish ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -10,4 +10,39 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+/*
+** [PROTOTYPE]
+** #include <string.h>
+** int	strncmp(const char *s1, const char *s2, size_t n);
+*/
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t		i;
+	unsigned char	*u1;
+	unsigned char	*u2;
+	
+	u1 = (unsigned char *)s1;
+	u2 = (unsigned char *)s2;
+	i = 0;
+	while (((s1[i] != '\0') || (s2[i] != '\0')) && (i < n))
+	{	
+		if (s1[i] != s2[i])
+			return (u1[i] - u2[i]);
+		i++;
+	}
+	return (0);
+}
+
+/*
+**int	main(void)
+**{
+**	printf("====ft_strncmp====\n");
+**	printf("%d\n%d\n", ft_strncmp("42Seoul_Libft", "42Seoul_libft", 10), ft_strncmp("42Seoul_Libft", "42Seoul", 10));
+**	printf("====strncmp====\n");
+**	printf("%d\n%d\n", strncmp("42Seoul_Libft", "42Seoul_libft", 10), strncmp("42Seoul_Libft", "42Seoul", 10));
+**	return (0);
+**}
+*/
