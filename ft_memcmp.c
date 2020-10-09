25d843e373c601478c2f2d134ed0b7071c08d6bb
TenaKim <tena.kim10@gmail.com>
2020-10-10 07:19:19 +0900

    Finish ft_memmove

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -10,4 +10,42 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include <string.h>
+/*
+** [PROTOTYPE]
+** #include <string.h>
+** int memcmp(const void *s1, const void *s2, size_t n);
+*/
 
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t		i;
+	unsigned char	*char_s1;
+	unsigned char	*char_s2;
+
+	if (n == 0)
+		return (0);
+	char_s1 = (unsigned char *)s1;
+	char_s2 = (unsigned char *)s2;
+
+	i = 0;
+	while (i < n)
+	{
+		if (char_s1[i] != char_s2[i])
+			return (char_s1[i] - char_s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+int	main(void)
+{
+	printf("====The memcmp() returns");
+	
+	printf("====ft_memcmp====\n");
+	printf("%d\n", ft_memcmp("42", "Seoul", 2));
+	printf("====memcmp====\n");
+	printf("%d\n", memcmp("42", "Seoul", 2));
+	return (0);
+}
--- ft_memmove.c
+++ ft_memmove.c
@@ -10,4 +10,43 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+/*
+** [PROTOTYPE]
+** #include <string.h>
+** void *memmove(void *dst, const void *src, size_t len);
+*/
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	size_t		i;
+	unsigned char	*char_dst;
+	unsigned char	*char_src;
+	unsigned char	temp_array[len];
+
+	char_dst = (unsigned char *)dst;
+	char_src = (unsigned char *)src;
+	i = 0;
+	while (i < len)
+	{
+		temp_array[i] = char_src[i];
+		char_dst[i] = temp_array[i];
+		i++;
+	}
+	return (dst);
+}
+
+/*
+**int	main(void)
+**{
+**	char	chars1[7];
+**	char	chars2[7];
+**
+**	printf("====ft_memmove====\n");
+**	printf("%s\n%p\n", chars1, ft_memmove(chars1, "Born2Code", sizeof(chars1)-1));
+**	printf("====memmove====\n");
+**	printf("%s\n%p\n", chars2, memmove(chars2, "Born2Code", sizeof(chars2)-1));
+**	return (0);
+**}
+*/
