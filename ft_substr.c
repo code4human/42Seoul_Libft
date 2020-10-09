f3e804979bedbf6acffcae6edac5e4b9a67a1a77
TenaKim <tena.kim10@gmail.com>
2020-10-10 07:53:26 +0900

    Finish ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -10,4 +10,37 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+/*
+** [PROTOTYPE]
+** char	*ft_substr(char const *s, unsigned int start, size_t len);
+*/
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	size_t	s_len;
+	char	*substring;
+
+	substring = (char *)malloc(sizeof(char) * (len + 1));
+	if ((s == NULL) || (!substring))
+		return (NULL);
+	s_len = ft_strlen(s);
+	i = 0;
+	while ((i < len) && (i + start < s_len))
+	{
+		substring[i] = s[start + i];
+		i++;
+	}
+	substring[i] = '\0';
+	return (substring);
+}
+
+/*
+**int	main(void)
+**{
+**	printf("====ft_substr====\n");
+**	printf("%s\n", ft_substr("BornToSeoul", 2, 6));
+**}
+*/
