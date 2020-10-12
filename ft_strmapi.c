7682e60bd439de8b94c972a33faa47ef7d657ab0
TenaKim <tena.kim10@gmail.com>
2020-10-13 04:32:25 +0900

    Finish ft_strmapi.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -10,4 +10,51 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+/*
+** [PROTOTYPE]
+** char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
+*/
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	unsigned int	s_len;
+	char		*result;
+
+	if ((s == NULL) || (f == NULL))
+		return (NULL);
+	s_len = ft_strlen(s);
+	if(!(result = (char *)malloc(sizeof(char) * (s_len + 1))))
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		result[i] = f(i, s[i]);
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
+
+/*
+**static char	ft_odd_index_upper(unsigned int n, char c)
+**{
+**	if (n % 2 != 0)
+**		if (c >= 'a' && c <= 'z')
+**			return (c - 32);
+**		else
+**			return (c);
+**	else
+**		return (c);
+**}
+**
+**int	main(void)
+**{
+**	printf("====ft_strmapi====\n");
+**	printf("====cAdEt42SeOuL====\n");
+**	printf("%s\n", ft_strmapi("cadet42seoul", ft_odd_index_upper));
+**	return (0);
+**}
+*/
