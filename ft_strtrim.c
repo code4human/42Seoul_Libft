e4f4d9e49a0514e32fb2ec8f21d84aa1e1d2b2eb
TenaKim <tena.kim10@gmail.com>
2020-10-12 10:04:21 +0900

    test ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -10,4 +10,64 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+/*
+** [PROTOTYPE]
+** char	*ft_strtrim(char const *s1, char const *set);
+**
+** [WARNING]
+** If you need subfunctions to write a complex function,
+** you should define these subfunctions as static to avoid publishing them
+** with your library.
+*/
+
+static	bool	ft_is_occur(char const *set, char const c)
+{
+	size_t	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (c == set[i])
+			return (true);
+		i++;
+	}
+	return (false);	
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	i;
+	size_t	len;
+	char *s2;
+
+	if (s1 == NULL)
+		return (NULL);
+	if (set ==  NULL)
+		return ((char *)s1);
+	len = ft_strlen(s1);
+	i = 0;
+	while (i < len && ft_is_occur(set, s1[i]))
+		i++;
+	while (len - 1 >= 0 && ft_is_occur(set, s1[len - 1]))
+		len--;
+	if ((len - 1) - i <= 0)
+		return (ft_strdup(""));
+	s2 = (char *)malloc(sizeof(char) * ((len - 1) - i) + 1);
+	if (!(s2))
+		return (NULL);
+	ft_strlcpy(s2, s1, (len - 1) - i);
+	s2[(len - 1) + i] = '\0';
+	return (s2);
+}
+
+int	main(void)
+{
+	printf("====ft_strtrim====\n");
+	printf("====BCDEF====\n");
+	printf("%s", ft_strtrim("ABCDEF", "EAC"));
+	printf("====BCD====\n");
+	printf("%s", ft_strtrim("ABCDEF", "AFE"));
+	return (0);
+}
