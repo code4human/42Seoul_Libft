c6820c9de203ec37d0ee1eb97b4619a32664dbb5
TenaKim <tena.kim10@gmail.com>
2020-10-13 02:30:47 +0900

    Finish ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -10,4 +10,60 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+/*
+** [PROTOTYPE]
+** char *ft_itoa(int n);
+*/
+
+static long	ft_abs(long nbr)
+{
+	return ((nbr < 0) ? -nbr : nbr);
+}
+
+static int	ft_len(long nbr)
+{
+	int	len;
+
+	len = (nbr <= 0) ? 1 : 0;
+	while (nbr != 0)
+	{
+		nbr = nbr / 10;
+		len++;
+	}
+	return (len);
+}
+
+char		*ft_itoa(int n)
+{
+	int		len;
+	int		sign;
+	char	*c;
+
+	sign = (n < 0) ? -1 : 1;
+	len = ft_len(n);
+	c = (char *)malloc(sizeof(char) * len + 1);
+	if (c == NULL)
+		return (NULL);
+	c[len] = '\0';
+	len--;
+	while (len >= 0)
+	{
+		c[len] = '0' + ft_abs(n % 10);
+		n = ft_abs(n / 10);
+		len--;
+	}
+	if (sign == -1)
+		c[0] = '-';
+	return (c);
+}
+
+/*
+**int main(void)
+**{
+**	printf("====ft_itoa====\n");
+**	printf("%s\n", ft_itoa(-42424242));
+**	return (0);
+**}
+*/
