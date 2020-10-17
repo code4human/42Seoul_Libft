87076252b2f99ac32a1a09d4ee103e47d056deb3
Taeeun Kim <taeekim@c2r5s1.42seoul.kr>
2020-10-17 16:47:39 +0900

    Insert 42header to file  and Modify header file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: taeekim <taeekim@42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 04:21:28 by taeekim           #+#    #+#             */
-/*   Updated: 2020/10/13 07:05:16 by taeekim          ###   ########.fr       */
+/*   Updated: 2020/10/17 16:47:03 by taeekim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 # include <stdlib.h>
 # include <stdbool.h>
 # include <unistd.h>
-# include <stdio.h>
 
 typedef	struct	s_list
 {
@@ -58,5 +57,14 @@ void		ft_putchar_fd(char c, int fd);
 void		ft_putstr_fd(char *s, int fd);
 void		ft_putendl_fd(char *s, int fd);
 void		ft_putnbr_fd(int n, int fd);
+t_list		*ft_lstnew(void	*content);
+void		ft_lstadd_front(t_list **lst, t_list *new);
+int			ft_lstsize(t_list *lst);
+t_list		*ft_lstlast(t_list *lst);
+void		ft_lstadd_back(t_list **lst, t_list *new);
+void		ft_lstdelone(t_list *lst, void (*del)(void *));
+void		ft_lstclear(t_list **lst, void (*del)(void *));
+void		ft_lstiter(t_list *lst, void (*f)(void *));
+t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
